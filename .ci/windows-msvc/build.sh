ad9194cc44f3eb8a25dab637b13d420151253205
kaleidobishbash <297662254+kaleidobishbash@users.noreply.github.com>
2026-06-28 17:18:46 +0000

    Update build.sh

--- .ci/windows-msvc/build.sh
+++ .ci/windows-msvc/build.sh
@@ -4,15 +4,17 @@ mkdir build && cd build
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -G Ninja \
-    -DCMAKE_TOOLCHAIN_FILE="$(pwd)/../CMakeModules/MSVCCache.cmake" \
-    -DCITRA_USE_CCACHE=ON \
-    -DENABLE_QT_TRANSLATION=OFF \
     -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} \
+    -DCITRA_USE_BUNDLED_QT=OFF \
+    -DCITRA_USE_CCACHE=ON \
+    -DCMAKE_TOOLCHAIN_FILE="$(pwd)/../CMakeModules/MSVCCache.cmake" \
     -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
-    -DUSE_DISCORD_PRESENCE=ON \
     -DENABLE_MF=ON \
-    -DENABLE_FFMPEG_VIDEO_DUMPER=ON \
-    -DOPENSSL_DLL_DIR="C:\Program Files\OpenSSL\bin"
+    -DENABLE_QT_TRANSLATION=OFF \
+    -DENABLE_SDL2=OFF \
+    -DENABLE_WEB_SERVICE=OFF \
+    -DOPENSSL_DLL_DIR="C:\Program Files\OpenSSL\bin" \
+    -DQt5_DIR="C:/Qt/Qt/Qt-5.15.19"
 
 ninja
 # show the caching efficiency
